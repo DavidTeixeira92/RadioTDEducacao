d9297b281c7c4f3d60c394ffad5bf6035e12c4ce
David Teixeira <divield@gmail.com>
2020-08-13 17:39:01 -0300

    03 níveis ok

---
+++ difícil/playerElements.js
@@ -0,0 +1,42 @@
+import { secondsToMinutes } from "./utils.js";
+
+export default {
+
+    get() {
+
+        this.cover = document.querySelector(".card-image");
+        this.title = document.querySelector(".card-content h5");
+        this.artist = document.querySelector(".artist");
+        // this.audio = document.querySelector("audio");
+        this.playPause = document.querySelector("#play-pause");
+        // esta linha se refere ao ícone de volume
+        this.mute = document.querySelector("#mute");
+        // esta linha se refere ao controle de volume
+        this.volume = document.querySelector("#vol-control");
+        this.seekbar = document.querySelector("#seekbar");
+        this.currentDuration = document.querySelector("#current-duration");
+        this.totalDuration = document.querySelector("#total-duration");
+    },
+    // Vamos criar o audio
+    createAudioElement(audio) {
+
+        this.audio = new Audio(audio);
+
+    },
+
+    actions() {
+        this.audio.onended = () => this.next();
+        this.audio.ontimeupdate = () => this.timeUpdate();
+        this.playPause.onclick = () => this.togglePlayPause();
+        this.mute.onclick = () => this.toggleMute();
+        this.volume.oninput = () => this.setVolume(this.volume.value);
+        this.volume.onchange = () => this.setVolume(this.volume.value);
+        this.seekbar.oninput = () => this.setSeek(this.seekbar.value);
+        this.seekbar.onchange = () => this.setSeek(this.seekbar.value);
+        this.seekbar.max = this.audio.duration;
+        this.totalDuration.innerText = secondsToMinutes(this.audio.duration);
+        
+    }
+
+};
+
